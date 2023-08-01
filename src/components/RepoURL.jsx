8b86b80603e116c04a01f72d0b68a0cf472a868b
GaboGomezT <gomezgabriel09@gmail.com>
2023-08-01 13:51:52 -0600

    added basic pagination

--- src/components/RepoURL.jsx
+++ src/components/RepoURL.jsx
@@ -3,10 +3,12 @@ import { Octokit } from "@octokit/rest";
 
 const octokit = new Octokit({ auth: import.meta.env.VITE_GITHUB_PAT });
 
-export default function RepoURL({ setIssues, issues }) {
+export default function RepoURL({ setIssues }) {
 	const [repoUrl, setRepoUrl] = useState("");
+	const [currentPage, setCurrentPage] = useState(1);
 
-	const getRepoIssues = async () => {
+	const getRepoIssues = async (page) => {
+		// Receive the page as an argument
 		const urlElements = repoUrl.split("/");
 		const owner = urlElements[urlElements.length - 2];
 		const repo = urlElements[urlElements.length - 1];
@@ -14,11 +16,25 @@ export default function RepoURL({ setIssues, issues }) {
 			const { data } = await octokit.rest.issues.listForRepo({
 				owner,
 				repo,
+				page: page,
+				per_page: 10,
 			});
 			setIssues(data);
 		} catch (error) {
 			console.error("Failed to fetch issues", error);
 		}
+	};
+
+	const handleNextPageClick = () => {
+		const nextPage = currentPage + 1;
+		setCurrentPage(nextPage);
+		getRepoIssues(nextPage);
+	};
+
+	const handlePrevPageClick = () => {
+		const prevPage = currentPage - 1;
+		setCurrentPage(prevPage);
+		getRepoIssues(prevPage);
 	};
 
 	return (
@@ -33,9 +49,33 @@ export default function RepoURL({ setIssues, issues }) {
 				required
 			/>
 
-			<button type="button" onClick={getRepoIssues}>
-				Get Issues
-			</button>
+			{currentPage === 1 && ( // Only show the 'Fetch Issues' button if the current page is 1
+				<button
+					type="button"
+					onClick={() => {
+						getRepoIssues(currentPage);
+						const nextPage = currentPage + 1;
+						setCurrentPage(nextPage);
+					}}
+				>
+					Get Issues
+				</button>
+			)}
+
+			{currentPage > 2 && ( // Only show the 'Previous Page' button if the current page is greater than 1
+				<button type="button" onClick={handlePrevPageClick}>
+					Previous Page
+				</button>
+			)}
+
+			{currentPage >= 2 && ( // Only show the 'Next Page' button if issues have been fetched at least once
+				<div>
+					<button type="button" onClick={handleNextPageClick}>
+						Next Page
+					</button>
+					<p>Page: {currentPage - 1}</p>
+				</div>
+			)}
 		</div>
 	);
 }
